1dd32c919989f7f58f0a3d0992c33df576dc859f
Koen Punt <koen@koenpunt.nl>
2015-08-05 11:09:55 +0200

    add dynamic hostname replacement
    by using the {hostname} placeholder in your pac file, you can easily
    make the proxy work for remote clients like your ipad or iphone

--- index.js
+++ index.js
@@ -4,14 +4,22 @@ var path = require('path');
 
 var HOME = process.env.HOME || process.env.USERPROFILE;
 
+function hostnameForRequest(req){
+  return req.headers['host'].split(':')[0];
+}
+
 var server = http.createServer(function(req, res){
   fs.readFile(path.join(HOME, '.proxy.pac'), function(err, file){
-	if(err){
-	  res.statusCode = 500;
-	  return res.end(err.toString());
-	}
-	res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
-	res.end(file);
+    if(err){
+      res.statusCode = 500;
+      return res.end(err.toString());
+    }
+    var contents = file.toString();
+    if(contents.indexOf('{hostname}')){
+      contents = contents.replace(/\{hostname\}/g, hostnameForRequest(req));
+    }
+    res.setHeader('Content-Type', 'application/x-ns-proxy-autoconfig');
+    res.end(contents);
   });
 });
 
